13a5cd9554dce7b3720982312329162a7a9603ee
Konrad Merz <kmerz@hulud.net>
2014-07-08 21:18:07 +0200

    Clean up code
    - prefer where before let
    - use guards instead of if cascading
    - don't use tabs, they are like ugh

--- s_world.hs
+++ s_world.hs
@@ -25,36 +25,35 @@ data World = World {
 
 moveHero :: World -> Input -> World
 moveHero world input = if legalMove
-			      then world{hero = (hero world) {
-				   position = newHeroPos }}
-			      else world
+  then world{hero = (hero world) { position = newHeroPos }}
+  else world
   where heroPos = position (hero world)
-	newHeroPos = newPos input heroPos
-	legalMove = not $ newHeroPos `elem` (wall world)
+        newHeroPos = newPos input heroPos
+        legalMove = not $ newHeroPos `elem` (wall world)
 
 moveMonster :: World -> ClockTime -> World
-moveMonster w tnow = let nextMove = calcMoveMonster w
-			 legalMove = legalMonsterMove nextMove tnow w
-	             in if legalMove
-			  then w{monster = (monster w){ mposition = nextMove,
-			         lastMove = tnow}}
-			  else w
-
+moveMonster w tnow = if legalMove
+  then w{monster = (monster w){ mposition = nextMove, lastMove = tnow }}
+  else w
+  where nextMove = calcMoveMonster w
+        legalMove = legalMonsterMove nextMove tnow w
 
 legalMonsterMove :: Coord -> ClockTime -> World -> Bool
-legalMonsterMove pos tnow w = let timediff = diffClockTimes tnow (lastMove (monster w))
-				  legalpos = (not $ pos `elem` (wall w)) &&
-					     (not $ pos == (position (hero w)))
-			     in legalpos && timediff > (TimeDiff 0 0 0 0 0 0 500000000000)
+legalMonsterMove pos tnow w = legalpos && timeSinceLastMove >= timeToNextMove
+  where timeSinceLastMove = diffClockTimes tnow (lastMove (monster w))
+        legalpos = notOnWall && notOnHero
+	notOnWall = (not $ pos `elem` (wall w))
+	notOnHero = (not $ pos == (position (hero w)))
+	timeToNextMove = (TimeDiff 0 0 0 0 0 0 500000000000)
 
 calcMoveMonster :: World -> Coord
-calcMoveMonster w = let heroPos = (position (hero w))
-		        monsterPos = (mposition (monster w))
-		        vector = subtractCoords heroPos monsterPos
-		in if (abs (fst vector)) > (abs (snd vector))
-		       then if (fst vector) > 0
-			       then addCoords monsterPos (1,  0)
-			       else addCoords monsterPos (-1, 0)
-		       else if (snd vector) > 0
-			       then addCoords monsterPos (0,  1)
-			       else addCoords monsterPos (0, -1)
+calcMoveMonster w
+  | abs x > abs y && x > 0 = addCoords monsterPos (1,  0)
+  | abs x > abs y && x <= 0 = addCoords monsterPos (-1, 0)
+  | abs x <= abs y && y > 0 = addCoords monsterPos (0,  1)
+  | abs x <= abs y && y <= 0 = addCoords monsterPos (0, -1)
+  where heroPos = (position (hero w))
+	monsterPos = (mposition (monster w))
+        vector = subtractCoords heroPos monsterPos
+	x = (fst vector)
+	y = (snd vector)
